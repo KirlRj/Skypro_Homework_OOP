c32b6837f89f9285d1003c5f2ae8f119d351416a
Kirill <kirill.rylskij96@gmail.com>
2026-01-28 17:09:59 +0300

    add new functional in category.py and product.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс категории продуктов. Структура: имя, описание, продукты"""
 
@@ -7,9 +10,21 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list) -> None:
+    def __init__(self, name: str, description: str, products: list | None = None) -> None:
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
+
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self) -> str | None:
+        products = ""
+        for product in self.__products:
+            products += f"{product.name}, {product.product_price} руб. Остаток:{product.quantity} шт\n"
+        return products
--- src/product.py
+++ src/product.py
@@ -6,8 +6,48 @@ class Product:
     price: float
     quantity: int
 
+    all_products: list[Product] = []
+
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def product_price(self) -> float:
+        return self.__price
+
+    @product_price.setter
+    def product_price(self, price: float) -> None:
+        if price <= 0:
+            raise ValueError("Цена не должна быть нулевая или отрицательная")
+
+        if price <= self.__price:
+            print(f"Новая цена {price} руб. меньше старой {self.__price} руб. Заменить цену? y/n")
+            q = input()
+            if q.lower() == "y":
+                self.__price = price
+                print("Цена обновлена")
+            else:
+                print("Цена остается прежней")
+        else:
+            self.__price = price
+
+    @classmethod
+    def new_product(cls, data: dict) -> "Product":
+        name = data["name"]
+        description = data["description"]
+        price = data["price"]
+        quantity = data["quantity"]
+
+        for product in cls.all_products:
+            if product.name == name:
+                product.quantity += quantity
+                if price > product.product_price:
+                    product.product_price = price
+                return product
+
+        new_product = cls(name, description, price, quantity)
+        cls.all_products.append(new_product)
+        return new_product
